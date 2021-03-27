b0e0d8b22c5c3a05649b946b3560219231fe010b
Billy Keating <wmontoothkeating@gmail.com>
2021-03-27 13:46:03 -0700

    Member model

---
+++ models/member.js
@@ -0,0 +1,44 @@
+// import important parts of sequelize library
+const { Model, DataTypes } = require('sequelize');
+// import our database connection from config.js
+const sequelize = require('../config/connection');
+
+// Initialize Member model (table) by extending off Sequelize's Member class
+class Member extends Model {}
+
+// set up fields and rules for Member model
+Member.init(
+  {
+    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    last_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    tier_id: {
+      type: DataTypes.INTERGER,
+      references: {
+        model: 'tier',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'member',
+  }
+);
+
+module.exports = Member;
