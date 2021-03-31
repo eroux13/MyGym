c45a6d2cea47f7508d5408a6512d0efcf0161e96
Billy Keating <wmontoothkeating@gmail.com>
2021-03-30 19:46:02 -0700

    Finished associations

--- models/associations.js
+++ models/associations.js
@@ -1,4 +1,44 @@
+// Imported models
+
 const Class = require('./class.js');
 const Member = require('./member.js');
 const Tier = require('./trainer.js');
 const Trainer = require('./trainer.js');
+
+// Classes belong to a trainer
+
+Class.belongsTo(Trainer, {
+    foreignKey: 'trainer_id',
+});
+
+// Trainers have many classes
+
+Trainer.hasMany(Class, {
+    foreignKey: 'trainer_id',
+    onDelete: 'CASCADE',
+})
+
+// Members have many classes
+
+Member.belongsToMany(Class, {
+    through: 'MemberClass'
+});
+
+// Classes have many members
+
+Class.belongsToMany(Member, {
+    through: 'MemberClass'
+});
+
+// Members have one tier
+
+Member.belongsTo(Tier, {
+    foreignKey: 'tier_id'
+})
+
+// Tiers have many members
+
+Tier.hasMany(Member, {
+    foreignKey: 'tier_id',
+    onDelete: 'CASCADE'
+})
