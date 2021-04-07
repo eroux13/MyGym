87cf0cd0708253b42f33ea4fc5d50ec8f8fe08c0
Maria Lara <74436232+maria-lara@users.noreply.github.com>
2021-04-06 19:21:32 -0700

    adding handlebars and public js/css

---
+++ public/js/member-login.js
@@ -0,0 +1,23 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (email && password) {
+        //Need member login route 
+        const response = await fetch('/member/login', {
+            method: 'POST',
+            body: JSON.stringify({ email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+        if (response.ok) {
+            document.location.replace('/member');
+        } else {
+            alert('Cannot log in');
+        }
+    }
+};
+
+document
+    .querySelector('.login-area')
+    .addEventListener('submit', loginFormHandler);
