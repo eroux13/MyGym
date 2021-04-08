d26ab64bb8737b9b5cf584a3033dc5db34ed59d0
Billy Keating <wmontoothkeating@gmail.com>
2021-04-07 19:14:39 -0700

    registration page working

--- models/member.js
+++ models/member.js
@@ -37,8 +37,25 @@ Member.init(
         key: 'id'
       }
     }, 
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
+    },
   },
   {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      },
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
