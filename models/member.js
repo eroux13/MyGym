e29bcc52b9c6ce7c7dd2cc11e9f784506ab83279
Arthur Leung <arthur.leung3@gmail.com>
2021-04-07 21:21:20 -0500

    Adding passport data

--- models/member.js
+++ models/member.js
@@ -36,9 +36,34 @@ Member.init(
         model: 'tier',
         key: 'id'
       }
-    }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
+    },
   },
   {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      },
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({ secret: 'supersupersecret', resave: true, saveUninitialized: true }));
-app.use(passport.intialize());
+app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(routes);
