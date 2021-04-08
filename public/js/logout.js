967db56c4b694fea71da1a38e2cc7cd459d7a90e
Maria Lara <74436232+maria-lara@users.noreply.github.com>
2021-04-07 19:11:56 -0700

    updated css link

---
+++ public/js/logout.js
@@ -0,0 +1,15 @@
+async function logout() {
+            //Need member logout route 
+    const response = await fetch('/member/logout', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  }
+  
+  document.querySelector('#logout').addEventListener('click', logout);
